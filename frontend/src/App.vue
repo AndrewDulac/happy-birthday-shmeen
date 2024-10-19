ae050b872a6534ee6d158a100003a903c64896eb
AndrewDulac <andrew.dulac4@gmail.com>
2024-10-19 17:31:08 -0500

    Revert that change

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -25,7 +25,6 @@ body {
   font-family: 'Poppins', sans-serif;
   color: #fff;
   text-shadow: .2vh .2vh .2vh rgba(0, 0, 0, 0.2);
-  height: 100vh;
-  overflow: hidden;
+  height: 80vh;
 }
 </style>
